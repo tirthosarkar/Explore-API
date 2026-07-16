ef7854c51827322feae7eb06ec3ec4fb629f21f5
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-16 23:47:13 +0600

    Added script.js const person

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Explore API")
+
+const person = {
+  name: "selim",
+  fruit: "dalim",
+  dish: "halim",
+  friends: ["alim", "kolim", "lamim"],
+  isRich: false,
+  money: 34000,
+};
+console.log(person);
